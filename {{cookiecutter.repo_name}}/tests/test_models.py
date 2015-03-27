5d602e7b318a8dd4c51faf24fa6f2c3cc42a4ad7
Dominic Rodger <dominicrodger+github@gmail.com>
2015-03-27 06:57:28 +0000

    Let's change the way we test to py.test

--- {{cookiecutter.repo_name}}/tests/test_models.py
+++ {{cookiecutter.repo_name}}/tests/test_models.py
@@ -8,20 +8,8 @@ test_{{ cookiecutter.repo_name }}
 Tests for `{{ cookiecutter.repo_name }}` models module.
 """
 
-import os
-import shutil
-import unittest
-
 from {{ cookiecutter.app_name }} import models
 
 
-class Test{{ cookiecutter.app_name|capitalize }}(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_something(self):
-        pass
-
-    def tearDown(self):
-        pass
+def test_something(self):
+    pass
