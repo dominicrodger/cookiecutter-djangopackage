4c199129415c9cc5d8bb41ea27bd41944bcfb36d
Dominic Rodger <dominicrodger+github@gmail.com>
2015-04-13 08:21:52 +0100

    Make setup.py test work

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -6,10 +6,21 @@ import sys
 
 import {{ cookiecutter.app_name }}
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 version = {{ cookiecutter.app_name }}.__version__
 readme = open('README.rst').read()
@@ -41,5 +52,12 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
+    tests_require=[
+        "pytest==2.6.4",
+        "pytest-cov==1.7.0",
+        "pytest-django==2.8.0",
+    ],
+    cmdclass = {'test': PyTest},
 )
