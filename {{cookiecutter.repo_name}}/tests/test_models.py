f7790b4db897cdff9c5a5f0c42c25a4525f7e0e0
Dominic Rodger <dominicrodger+github@gmail.com>
2015-04-13 17:40:03 +0100

    Comments here probably aren't hugely helpful

--- {{cookiecutter.repo_name}}/tests/test_models.py
+++ {{cookiecutter.repo_name}}/tests/test_models.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-test_{{ cookiecutter.repo_name }}
-------------
-
-Tests for `{{ cookiecutter.repo_name }}` models module.
-"""
-
-from {{ cookiecutter.app_name }} import models
-
 
 def test_something():
     pass
