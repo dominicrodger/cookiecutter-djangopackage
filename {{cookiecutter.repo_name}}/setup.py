c74caa8f98cb104aabee10043ed4eec8e0c98c4e
Dominic Rodger <dominicrodger+github@gmail.com>
2015-04-13 08:21:38 +0100

    Remove ability to publish via setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -12,14 +12,6 @@ except ImportError:
     from distutils.core import setup
 
 version = {{ cookiecutter.app_name }}.__version__
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (version, version))
-    print("  git push --tags")
-    sys.exit()
-
 readme = open('README.rst').read()
 
 setup(
